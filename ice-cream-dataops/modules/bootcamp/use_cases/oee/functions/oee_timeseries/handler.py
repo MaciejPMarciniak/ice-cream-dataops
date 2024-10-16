d8608a725a6a2a9979949d39f2408afb7545fc03
Maciej Marciniak <maciej.marciniak@cognite.com>
2024-10-16 13:37:39 +0200

    Moving to prod

--- ice-cream-dataops/modules/bootcamp/use_cases/oee/functions/oee_timeseries/handler.py
+++ ice-cream-dataops/modules/bootcamp/use_cases/oee/functions/oee_timeseries/handler.py
@@ -3,6 +3,7 @@ from datetime import timedelta
 from typing import Any, Dict
 
 import numpy as np
+import pandas as pd
 from cognite.client import CogniteClient
 from cognite.client.data_classes import TimeSeries
 from cognite.client.exceptions import CogniteNotFoundError
@@ -10,42 +11,68 @@ from retry import retry
 
 
 def handle(client: CogniteClient, data: Dict[str, Any] = {}) -> None:
-    lookback_minutes = timedelta(minutes=data.get("lookback_minutes", 60)).total_seconds() * 1000
+    lookback_minutes = (
+        timedelta(minutes=data.get("lookback_minutes", 60)).total_seconds() * 1000
+    )
     data_set_external_id = data.get("data_set_external_id", "ds_uc_oee")
     data_set_id = client.data_sets.retrieve(external_id=data_set_external_id).id
-    all_sites = ["Oslo", "Houston", "Kuala Lumpur", "Hannover", "Nuremberg", "Marseille", "Sao Paulo", "Chicago", "Rotterdam", "London"]
+    all_sites = [
+        "Oslo",
+        "Houston",
+        "Kuala Lumpur",
+        "Hannover",
+        "Nuremberg",
+        "Marseille",
+        "Sao Paulo",
+        "Chicago",
+        "Rotterdam",
+        "London",
+    ]
     sites = data.get("sites", all_sites)
 
     print(f"Processing datapoints for these sites: {sites}")
     with ThreadPoolExecutor(max_workers=10) as executor:
-        futures = [executor.submit(process_site, client, data_set_id, lookback_minutes, site) for site in sites]
+        futures = [
+            executor.submit(process_site, client, data_set_id, lookback_minutes, site)
+            for site in sites
+        ]
         for f in futures:
             f.result()
+
 
 @retry(tries=5, delay=5)
 def process_site(client, data_set_id, lookback_minutes, site):
     assets = client.assets.list(asset_subtree_external_ids=site.lower(), limit=None)
-    timeseries = client.time_series.list(asset_subtree_external_ids=site.lower(), limit=None)
+    timeseries = client.time_series.list(
+        asset_subtree_external_ids=site.lower(), limit=None
+    )
     external_ids = [ts.external_id for ts in timeseries]
     all_latest_dps = client.time_series.data.retrieve_latest(external_id=external_ids)
 
     # Organize latest datapoints by equipment for alignment
     assets_dps = {
-        asset.external_id: [latest_dp for latest_dp in all_latest_dps if asset.external_id in latest_dp.external_id]
+        asset.external_id: [
+            latest_dp
+            for latest_dp in all_latest_dps
+            if asset.external_id in latest_dp.external_id
+        ]
         for asset in assets
     }
 
     for asset, latest_dps in assets_dps.items():
-        end = min([dp.timestamp[0] for dp in latest_dps if latest_dps and dp.timestamp], default=None)
+        end = min(
+            [dp.timestamp[0] for dp in latest_dps if latest_dps and dp.timestamp],
+            default=None,
+        )
         if end:
-            dps_df = client.time_series.data.retrieve_dataframe(
+            dps_df: pd.DataFrame = client.time_series.data.retrieve_dataframe(
                 external_id=[dp.external_id for dp in latest_dps],
                 start=end - lookback_minutes,
                 end=end,
                 aggregates=["sum"],
                 granularity="1m",
                 include_aggregate_name=False,
-                limit=None
+                limit=None,
             )
 
             # Frontfill because "planned_status" and "status" only have datapoints when the value changes
@@ -53,17 +80,25 @@ def process_site(client, data_set_id, lookback_minutes, site):
 
             # Fill the rest with the opposite
             try:
-                first_valid_value = dps_df[f"{asset}:planned_status"].loc[dps_df[f"{asset}:planned_status"].first_valid_index()]
+                first_valid_value = dps_df[f"{asset}:planned_status"].loc[
+                    dps_df[f"{asset}:planned_status"].first_valid_index()
+                ]
             except:
                 print(f"Failed to find datapoints for {asset}:planned_status")
                 continue
             backfill_value = 1.0 if first_valid_value == 0.0 else 0.0
-            dps_df[f"{asset}:planned_status"] = dps_df[f"{asset}:planned_status"].fillna(value=backfill_value)
+            dps_df[f"{asset}:planned_status"] = dps_df[
+                f"{asset}:planned_status"
+            ].fillna(value=backfill_value)
 
             # Same for status
-            first_valid_value = dps_df[f"{asset}:status"].loc[dps_df[f"{asset}:status"].first_valid_index()]
+            first_valid_value = dps_df[f"{asset}:status"].loc[
+                dps_df[f"{asset}:status"].first_valid_index()
+            ]
             backfill_value = 1.0 if first_valid_value == 0.0 else 0.0
-            dps_df[f"{asset}:status"] = dps_df[f"{asset}:status"].fillna(value=backfill_value)
+            dps_df[f"{asset}:status"] = dps_df[f"{asset}:status"].fillna(
+                value=backfill_value
+            )
 
             count_dps = dps_df[f"{asset}:count"]
             good_dps = dps_df[f"{asset}:good"]
@@ -90,14 +125,23 @@ def process_site(client, data_set_id, lookback_minutes, site):
             dps_df[f"{asset}:performance"] = (count_dps / status_dps) / (60.0 / 3.0)
             dps_df[f"{asset}:availability"] = status_dps / planned_status_dps
 
-            dps_df[f"{asset}:oee"] = dps_df[f"{asset}:quality"] * dps_df[f"{asset}:performance"] * dps_df[f"{asset}:availability"]
+            dps_df[f"{asset}:oee"] = (
+                dps_df[f"{asset}:quality"]
+                * dps_df[f"{asset}:performance"]
+                * dps_df[f"{asset}:availability"]
+            )
 
             # Fill in the divide by zeros
             dps_df = dps_df.fillna(value=0.0)
             dps_df = dps_df.replace([np.inf, -np.inf], 0.0)
 
             # Drop input timeseries
-            dps_df = dps_df.drop(columns=[f"{asset}:{postfix}" for postfix in ["good", "count", "status", "planned_status"]])
+            dps_df = dps_df.drop(
+                columns=[
+                    f"{asset}:{postfix}"
+                    for postfix in ["good", "count", "status", "planned_status"]
+                ]
+            )
 
             try:
                 client.time_series.data.insert_dataframe(dps_df)
@@ -118,7 +162,7 @@ def process_site(client, data_set_id, lookback_minutes, site):
                             external_id=external_id,
                             name=" ".join(name),
                             metadata={"type": typ},
-                            data_set_id=data_set_id
+                            data_set_id=data_set_id,
                         )
                     )
 
